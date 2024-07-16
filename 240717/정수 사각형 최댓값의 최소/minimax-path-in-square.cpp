00102572d1c03d0c4da1828eacb622a000a8db85
kimbab2066 <100542459+kimbab2066@users.noreply.github.com>
2024-07-17 08:04:24 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#define MX 105
+int a[MX][MX], d[MX][MX];
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++) cin >> a[i][j];
+    }
+    d[1][1] = a[1][1];
+    for(int i = 2; i <= n; i++){
+        d[i][1] = max(d[i-1][1], a[i][1]);
+    }
+    for(int i = 2; i <= n; i++){
+        d[1][i] = max(d[1][i-1], a[1][i]);
+    }
+
+    for(int i = 2; i <= n; i++){
+        for(int j = 2; j <= n; j++){
+            d[i][j] = max(min(d[i-1][j], d[i][j-1]), a[i][j]);
+        }
+    }
+
+    //   for(int i = 1; i <= n; i++){
+    //     for(int j = 1; j <= n; j++) cout << d[i][j] << " ";
+    //     cout << "\n";
+    // }
+
+
+    cout << d[n][n];
+
+    return 0;
+}
