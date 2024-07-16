4ed1232406914c31df111f4e1c817c089679945b
kimbab2066 <100542459+kimbab2066@users.noreply.github.com>
2024-07-17 07:15:49 +0900

    Title: 정수 사각형 최대 합, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define MX 105
+int a[MX][MX], d[MX][MX];
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++)cin >> a[i][j];
+    }
+    d[1][1] = a[1][1];
+    for(int i = 2; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            d[i][j] = max(d[i-1][j] + a[i][j], d[i][j-1] + a[i][j]);
+        }
+    }
+    cout << d[n][n];
+
+    return 0;
+}
