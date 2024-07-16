00f82ea614b80196f62aa95e546ed69295d2f040
kimbab2066 <100542459+kimbab2066@users.noreply.github.com>
2024-07-17 07:47:16 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240717/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#define MX 105
+int a[MX][MX], d[MX][MX];
+
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++) cin >> a[i][j];
+    }
+    d[1][1] = a[1][1];
+    for(int i = 1; i <= n; i++){
+        d[i][1] = a[i][1];
+        d[1][i] = a[1][i];
+    }
+    for(int i = 2; i <= n; i++){
+        for(int j = 2; j <= n; j++){
+            d[i][j] = min(max(d[i-1][j], d[i][j-1]), a[i][j]);
+        }
+    }
+    cout << d[n][n];
+
+    return 0;
+}
